fc01839ec6e2734a13fd48d338dcd52747443847
André Santos <andrefs@cpan.org>
2022-10-23 20:24:46 +0100

    add examples

---
+++ examples/trie.rs
@@ -0,0 +1,11 @@
+use trie::Trie;
+
+fn main() {
+    let mut t = Trie::<i32>::new(None);
+
+    t.add("https://google.com", Some(1));
+    t.add("http://wikipedia.org", Some(1));
+    t.add("https://imdb.com", Some(1));
+
+    println!("{}", t.pp());
+}
